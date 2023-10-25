d5c4828f174aa18e352f8cdc6dc3c377bfca7a38
KiSStaa <112769455+KiSStaa@users.noreply.github.com>
2023-10-25 17:38:18 +0400

    davaleba 1

---
+++ index.py
@@ -0,0 +1,24 @@
+# davaleba 1
+
+num1 = int(input('First Number: '))
+num2 = int(input('Second Number: '))
+
+print('Res: ', num1 + num2)
+print('Res: ', num1 - num2)
+print('Res: ', num1 * num2)
+print('Res: ', num1 / num2)
+
+# davaleba 2
+
+name = str(input('Please Enter Your Name: '))
+print('Hallo '+ name)
+
+# davaleba 3
+
+rad = int(input("Please Enter circle's radius: "))
+print("circle's diameter: ", rad * 2)
+
+# davaleba 4
+
+col = str(input('Please Enter Your Fav Color: '))
+print('Hallo '+ name+','+col, 'is good choice')
